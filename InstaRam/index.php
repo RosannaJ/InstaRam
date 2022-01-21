04158bfda91f22f88d9d71332fe951dc66bccef8
Cypress Kouri <72818091+CypressKouri@users.noreply.github.com>
2022-01-21 14:22:03 -0800

    Update index.php

--- InstaRam/index.php
+++ InstaRam/index.php
@@ -1,9 +1,5 @@
 <?php
-
     session_start();
-
-     // show header
-    include "header.inc";
     include "functions.php";
 
     // process form data if submitted
@@ -60,8 +56,9 @@
         $_SESSION["page"] = 1;
     } // if
 
-   
     // show page content
+    include "header.inc";
+
     if ($_SESSION["page"] == 1) {	
         include "loginform.inc";	
     } else if ($_SESSION["page"] == 2) {	
@@ -85,5 +82,3 @@
     include "footer.inc";
 
 ?>
-
-
